002a9a8fca12686456cc27c7fd3ebda9eaf96c51
Anurag Raut <129861067+Anurag-raut21@users.noreply.github.com>
2023-08-28 20:16:13 +0530

    questions.js
    Javascript File for all questions inside cricket quizz

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who won the first ever Cricket World Cup in 1975?",
+    answer: "West Indies",
+    options: [
+      "Australia",
+      "England",
+      "India",
+      "West Indies"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the largest cricket stadium in the world?",
+    answer: "Narendra Modi Stadium",
+    options: [
+      "Lords",
+      "Eden Gardens",
+      "Narendra Modi Stadium",
+      "Melbourne Cricket Club"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How many ways are there of getting out?",
+    answer: "10",
+    options: [
+      "6",
+      "10",
+      "7",
+      "4"
+    ]
+  },
+    {
+    numb: 4,
+    question:"Where will the 2023 Cricket World Cup be hosted?",
+    answer: "India",
+    options: [
+      "England",
+      "India",
+      "New Zealand",
+      "South Africa"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the length of the cricket pitch?",
+    answer: "22",
+    options: [
+      "20.12",
+      "22",
+      "21.12",
+      "22.5"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
